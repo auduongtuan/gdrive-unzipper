9046bdc805ac45d68141dcbb3a02678475e01306
Tuan Au Duong <audtuan@gmail.com>
2023-07-31 14:00:36 +0700

    Initial Commit

---
+++ GDrive Unzip/GDrive_UnzipApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDrive_UnzipApp.swift
+//  GDrive Unzip
+//
+//  Created by Tuan on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct GDrive_UnzipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
